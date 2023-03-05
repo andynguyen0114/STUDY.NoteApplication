92e57f1488cdc56efb19dc66f50742452e0c561f
Andy Nguyen <andynguyen0114@gmail.com>
2023-03-05 15:02:08 -0600

    Add project files.

---
+++ NoteApp/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace NoteApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtBoxMessage = new System.Windows.Forms.TextBox();
+            this.lblMessageList = new System.Windows.Forms.Label();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.dataMessage = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataMessage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(273, 301);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(498, 301);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(41, 33);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(33, 13);
+            this.lblTitle.TabIndex = 2;
+            this.lblTitle.Text = "Title: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Message:";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(80, 30);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(186, 20);
+            this.txtTitle.TabIndex = 4;
+            // 
+            // txtBoxMessage
+            // 
+            this.txtBoxMessage.Location = new System.Drawing.Point(80, 64);
+            this.txtBoxMessage.Multiline = true;
+            this.txtBoxMessage.Name = "txtBoxMessage";
+            this.txtBoxMessage.Size = new System.Drawing.Size(268, 225);
+            this.txtBoxMessage.TabIndex = 5;
+            // 
+            // lblMessageList
+            // 
+            this.lblMessageList.AutoSize = true;
+            this.lblMessageList.Location = new System.Drawing.Point(381, 48);
+            this.lblMessageList.Name = "lblMessageList";
+            this.lblMessageList.Size = new System.Drawing.Size(72, 13);
+            this.lblMessageList.TabIndex = 7;
+            this.lblMessageList.Text = "Message List ";
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(80, 301);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(75, 23);
+            this.btnNew.TabIndex = 8;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(384, 301);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 9;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // dataMessage
+            // 
+            this.dataMessage.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataMessage.Location = new System.Drawing.Point(384, 64);
+            this.dataMessage.Name = "dataMessage";
+            this.dataMessage.Size = new System.Drawing.Size(199, 225);
+            this.dataMessage.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 390);
+            this.Controls.Add(this.dataMessage);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.lblMessageList);
+            this.Controls.Add(this.txtBoxMessage);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataMessage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtBoxMessage;
+        private System.Windows.Forms.Label lblMessageList;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.DataGridView dataMessage;
+    }
+}
+
---
+++ NoteApp/Form1.cs
@@ -0,0 +1,84 @@
+﻿/*
+ * An application used to store notes using Window Forms 
+ * User can:
+ *      - create a title and message for note, and save 
+ *      - preview created notes 
+ *      - delete created notes 
+ *      - create new notes 
+ */
+
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace NoteApp
+{
+    public partial class Form1 : Form
+    {
+        DataTable table;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            // a DataTable object 
+            table = new DataTable();
+
+            // add two columns to table  
+            table.Columns.Add("Title", typeof(string));
+            table.Columns.Add("Messages", typeof(string));
+
+            // connect table object to dataGrid
+            dataMessage.DataSource = table;
+        }
+
+        // clear former note to create new note 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtTitle.Clear();
+            txtBoxMessage.Clear();
+
+        }
+
+        // save note title and message into dataMessage 
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            table.Rows.Add(txtTitle.Text, txtBoxMessage.Text);
+
+            txtTitle.Clear();
+            txtBoxMessage.Clear();
+        }
+
+        // select created note to read 
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            // find which row is selected 
+            int index = dataMessage.CurrentCell.RowIndex;
+
+            // if any row is selected 
+            if (index > -1)
+            {
+                // display title
+                txtTitle.Text = table.Rows[index].ItemArray[0].ToString();
+
+                // display message 
+                txtBoxMessage.Text = table.Rows[index].ItemArray[1].ToString();
+            }
+        }
+
+        // delete a created note 
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            // if a row is selected 
+            int index = dataMessage.CurrentCell.RowIndex;
+
+            // delete row 
+            table.Rows[index].Delete();
+
+        }
+    }
+}
